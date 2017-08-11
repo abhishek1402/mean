cbc85bf918c1d9a70c8202e502bf3e9ff4d59d10
Abhi-Qexon <abhisheks@qexon.com>
2017-08-11 18:44:12 +0530

    removed message component frm app routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import {AppComponent} from './app.component';
 import {loginComponent} from './login/login.component';
 import {homeComponent} from './home/home.component';
 import {goalComponent} from './home/goal/goal.component';
-import {messagesComponent} from './home/messages/messages.Component';
+
 import {profileComponent} from './home/personalprofile/profile.component';
 
 
@@ -17,7 +17,6 @@ export const router:Routes = [
     { path:'home',component:homeComponent,
     children: [
       { path: 'goal', component:goalComponent },
-      { path:'messages',component:messagesComponent},
       { path:'profile',component:profileComponent}
     ]
     }
